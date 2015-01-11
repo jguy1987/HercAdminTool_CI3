33f55040c72868f7fc6723f186a3180649e783a9
John Mish <jmish1987@gmail.com>
2015-01-11 13:59:41 -0600

    Switched all pages over to use db_ragnarok from MY_Controller

--- application/models/adminmodel.php
+++ application/models/adminmodel.php
@@ -1,30 +1,30 @@
 <?php
 Class Adminmodel extends CI_Model {
 	function list_admins() {
-		$this->db_admin->select('*, hat_groups.name AS group_name, hat_users.id AS userid');
-		$this->db_admin->from('hat_users')->order_by('hat_users.id','asc');
-		$this->db_admin->join('hat_groups', 'hat_users.groupid = hat_groups.id');
-		$query = $this->db_admin->get();
+		$this->db_ragnarok->select('*, hat_groups.name AS group_name, hat_users.id AS userid');
+		$this->db_ragnarok->from('hat_users')->order_by('hat_users.id','asc');
+		$this->db_ragnarok->join('hat_groups', 'hat_users.groupid = hat_groups.id');
+		$query = $this->db_ragnarok->get();
 		return $query->result();
 	}
 	
 	function list_groups() {
-		$this->db_admin->select('id, name');
-		$this->db_admin->from('hat_groups')->order_by('id','asc');
-		$query = $this->db_admin->get();
+		$this->db_ragnarok->select('id, name');
+		$this->db_ragnarok->from('hat_groups')->order_by('id','asc');
+		$query = $this->db_ragnarok->get();
 		return $query->result_array();
 	}
 	
 	function list_users_in_group($groupid) {
-		$this->db_admin->select('hat_users.username, hat_users.id, hat_users.groupid');
-		$this->db_admin->from('hat_users')->order_by('hat_users.username','asc');
-		$this->db_admin->where('hat_users.groupid', $groupid);
-		$query = $this->db_admin->get();
+		$this->db_ragnarok->select('hat_users.username, hat_users.id, hat_users.groupid');
+		$this->db_ragnarok->from('hat_users')->order_by('hat_users.username','asc');
+		$this->db_ragnarok->where('hat_users.groupid', $groupid);
+		$query = $this->db_ragnarok->get();
 		return $query->result_array();
 	}
 	
 	function get_user_data($userid) {
-		$query = $this->db_admin->get_where('hat_users', array('id' => $userid));
+		$query = $this->db_ragnarok->get_where('hat_users', array('id' => $userid));
 		return $query->row();
 	}
 	
@@ -39,13 +39,13 @@ Class Adminmodel extends CI_Model {
 			}
 			$newPassMD5 = md5($newPass);
 			unset($data['genpass']);
-			$this->db_admin->where('id', $data['id']);
-			$this->db_admin->update('hat_users',array('passwd' => $newPassMD5));
+			$this->db_ragnarok->where('id', $data['id']);
+			$this->db_ragnarok->update('hat_users',array('passwd' => $newPassMD5));
 		}
 		// Update the data:
 		unset($data['genpass']);
-		$this->db_admin->where('id', $data['id']);
-		$this->db_admin->update('hat_users', $data);
+		$this->db_ragnarok->where('id', $data['id']);
+		$this->db_ragnarok->update('hat_users', $data);
 		if (isset($newPass)) {
 			return $newPass;
 		}
@@ -57,13 +57,13 @@ Class Adminmodel extends CI_Model {
 			case "lock": // Lock all hat_users
 				$query = array(
 					'disablelogin' => 1);
-				$this->db_admin->where('id <>', $userid);
-				$this->db_admin->update('hat_users', $query);
+				$this->db_ragnarok->where('id <>', $userid);
+				$this->db_ragnarok->update('hat_users', $query);
 				break;
 			case "unlock": // Unlock all hat_users
 				$query = array(
 					'disablelogin' => 0);
-				$this->db_admin->update('hat_users', $query);
+				$this->db_ragnarok->update('hat_users', $query);
 				break;
 		}
 	}
@@ -71,10 +71,10 @@ Class Adminmodel extends CI_Model {
 	function addgroup($data) {
 		// First get the list of permissions we need to insert into the database and set all those values to 1 so that we can insert them
 		array_fill_keys($data['perms'], 1);
-		$this->db_admin->set('id', $data['id']);
-		$this->db_admin->set('name', $data['name']);
-		$this->db_admin->set($data['perms']);
-		$this->db_admin->insert('hat_groups');
+		$this->db_ragnarok->set('id', $data['id']);
+		$this->db_ragnarok->set('name', $data['name']);
+		$this->db_ragnarok->set($data['perms']);
+		$this->db_ragnarok->insert('hat_groups');
 	}
 		
 	function addadminuser($data) {
@@ -85,22 +85,22 @@ Class Adminmodel extends CI_Model {
 		}
 		$newPassMD5 = md5($newPass);
 		
-		$this->db_admin->set('username', $data['username']);
-		$this->db_admin->set('pemail', $data['pemail']);
-		$this->db_admin->set('groupid', $data['groupid']);
+		$this->db_ragnarok->set('username', $data['username']);
+		$this->db_ragnarok->set('pemail', $data['pemail']);
+		$this->db_ragnarok->set('groupid', $data['groupid']);
 		if (isset($data['gameacctid'])) {
-			$this->db_admin->set('gameacctid', $data['gameacctid']);
+			$this->db_ragnarok->set('gameacctid', $data['gameacctid']);
 		}
-		$this->db_admin->set('passwd', $newPassMD5);
-		$this->db_admin->set('disablelogin', '1');
-		$this->db_admin->set('createdate', 'NOW()', FALSE);
-		$this->db_admin->insert('hat_users');
+		$this->db_ragnarok->set('passwd', $newPassMD5);
+		$this->db_ragnarok->set('disablelogin', '1');
+		$this->db_ragnarok->set('createdate', 'NOW()', FALSE);
+		$this->db_ragnarok->insert('hat_users');
 		return $newPass;
 	}
 	
 	function resetallpwd() {
-		$this->db_admin->select('id, username, pemail');
-		$q = $this->db_admin->get('hat_users');
+		$this->db_ragnarok->select('id, username, pemail');
+		$q = $this->db_ragnarok->get('hat_users');
 		$passwd = array();
 		foreach ($q->result_array() as $r) {
 			$chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%&';
@@ -110,8 +110,8 @@ Class Adminmodel extends CI_Model {
 			}
 			$newPassMD5 = md5($newPass);
 			
-			$this->db_admin->where('id', $r['id']);
-			$this->db_admin->update('hat_users', array('passwd' => $newPassMD5));
+			$this->db_ragnarok->where('id', $r['id']);
+			$this->db_ragnarok->update('hat_users', array('passwd' => $newPassMD5));
 			$passwd += array(
 				$r['id'] => array(
 					'username' 	=> $r['username'],
--- application/models/usermodel.php
+++ application/models/usermodel.php
@@ -1,13 +1,13 @@
 <?php
 Class Usermodel extends CI_Model {
 	function login($username, $passwd) {
-		$this->db_admin->select('id, username, passwd, groupid, lastlogin, disablelogin');
-		$this->db_admin->from('hat_users');
-		$this->db_admin->where('username', $username);
-		$this->db_admin->where('passwd', MD5($passwd));
-		$this->db_admin->limit(1);
+		$this->db_ragnarok->select('id, username, passwd, groupid, lastlogin, disablelogin');
+		$this->db_ragnarok->from('hat_users');
+		$this->db_ragnarok->where('username', $username);
+		$this->db_ragnarok->where('passwd', MD5($passwd));
+		$this->db_ragnarok->limit(1);
 	 
-		$query = $this->db_admin->get();
+		$query = $this->db_ragnarok->get();
 	 
 		if($query->num_rows() == 1) {
 		  return $query->result();
@@ -18,15 +18,15 @@ Class Usermodel extends CI_Model {
 	}
 	
 	function update_user_module($userid,$module) {
-		$this->db_admin->where('id', $userid);
-		$this->db_admin->update('hat_users', array('lastmodule' => $module));
+		$this->db_ragnarok->where('id', $userid);
+		$this->db_ragnarok->update('hat_users', array('lastmodule' => $module));
 	}
 	
 	function update_active($uid) {
 		// Update the active date to now.
-		$this->db_admin->where('id', $uid);
-		$this->db_admin->set('lastlogin', 'NOW()', FALSE);
-		$this->db_admin->update('hat_users');
+		$this->db_ragnarok->where('id', $uid);
+		$this->db_ragnarok->set('lastlogin', 'NOW()', FALSE);
+		$this->db_ragnarok->update('hat_users');
 	}
 }
 ?>
