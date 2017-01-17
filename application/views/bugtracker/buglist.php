a2dda1ca1a60bf173b6c5f3a803faf85b79bbf8b
John Mish <jmish1987@gmail.com>
2017-01-17 16:06:16 -0600

    Expanded the title column so there's more room and most title's don't line break. Fixes #28.

--- application/views/bugtracker/buglist.php
+++ application/views/bugtracker/buglist.php
@@ -8,23 +8,23 @@
 	</div>
 	<div class="panel-body">
 		<a href="/bugtracker/newbug" class="btn btn-info">Submit Bug</a>&nbsp;<a href="/bugtracker/config" class="btn btn-warning">Config Tracker</a><br /><br />
-		<button class="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseBugsearch" aria-controls="collapseBugsearch">Search Bugs</button><br />
+		<button class="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseBugsearch" aria-controls="collapseBugsearch">Filter Bugs</button><br />
 		<div class="collapse" id="collapseBugsearch">
 			<div class="card card-block">
-				search forms
+				<?php echo form_open('/bugtracker/buglist', array('class' => 'form-inline')); ?>
 			</div>
 		</div>
 		<table class="table table-striped table-bordered table-hover" id="dataTables-listlg">
 			<thead>
 				<tr>
 					<th style="width: 75px;">Bug ID</th>
-					<th style="width: 100px;">Title</th>
+					<th style="width: 225px;">Title</th>
 					<th style="width: 30px;">Reporter</th>
-					<th style="width: 150px;">Server</th>
+					<th style="width: 100px;">Server</th>
 					<th style="width: 100px;">Date Started</th>
 					<th style="width: 50px;">Version</th>
 					<th style="width: 75px;">Status</th>
-					<th style="width: 100px;">Priority</th>
+					<th style="width: 80px;">Priority</th>
 					<th style="width: 100px;">Category</th>
 					<th style="width: 100px;">Resolution</th>
 				</tr>
