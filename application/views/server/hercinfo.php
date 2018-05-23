5a931ed58d505cc6d8f3e5052c112c967f657d1d
John Mish <jmish1987@gmail.com>
2018-05-23 15:30:47 -0500

    Follow up to 56b5054843da5e9fdce0f2c88262c501bf7d613b Fixed some issues in hercinfo.php view.

--- application/views/server/hercinfo.php
+++ application/views/server/hercinfo.php
@@ -12,20 +12,13 @@
 						<i class="fa fa-bar-chart-o fa-fw"></i> Server Status <div class="small">(Use the buttons to toggle the state of that server)</div>
 					</div>
 					<div class="panel-body">
-						<?php if ($online_status['login'] == false) { ?> 
-							<a href="<?php echo base_url('maintenance/toggle/login'); ?>"><button class="btn btn-danger">Login</button></a>
-						<?php } else { ?>
-							<a href="<?php echo base_url('maintenance/toggle/login'); ?>"><button class="btn btn-success">Login</button></a>
-						<?php } ?>
-						<?php if ($online_status['char'] == false) { ?>
-							<a href="<?php echo base_url('maintenance/toggle/char'); ?>"><button class="btn btn-danger">Character</button></a>
-						<?php } else { ?>
-							<a href="<?php echo base_url('maintenance/toggle/char'); ?>"><button class="btn btn-success">Character</button></a>
-						<?php } ?>
-						<?php if ($online_status['map'] == false) { ?>
-							<a href="<?php echo base_url('maintenance/toggle/map'); ?>"><button class="btn btn-danger">Map</button></a>
-						<?php } else { ?>
-							<a href="<?php echo base_url('maintenance/toggle/map'); ?>"><button class="btn btn-success">Map</button></a>
+						<?php foreach ($online_status as $k=>$v) { ?>
+							<?php if ($v == false) { ?> 
+								<?php $buttonStatus = "danger"; ?>
+							<?php } else { ?>
+								<?php $buttonStatus = "success"; ?>
+							<?php } ?>
+							<a href="<?php echo base_url('server/maintenance/toggle/'.$k.''); ?>"><button class="btn btn-<?php echo $buttonStatus; ?>"><?php echo $k; ?></button></a>
 						<?php } ?>
 					</div>
 				</div>
@@ -36,13 +29,13 @@
 						<i class="fa fa-bar-chart-o fa-fw"></i> Server Maintenance
 					</div>
 					<div class="panel-body">
-						<a href="<?php echo base_url('maintenance/start'); ?>"><button type="button" class="btn btn-success <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Start Server</button></a>
-						<a href="<?php echo base_url('maintenance/stop'); ?>"><button type="button" class="btn btn-danger <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Stop Server</button></a>
-						<a href="<?php echo base_url('maintenance/restart'); ?>"><button type="button" class="btn btn-warning <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Restart Server</button></a><br /><br />
-						<a href="<?php echo base_url('maintenance/updatefiles'); ?>"><button type="button" class="btn btn-info <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Update Files</button></a><br /><br />
-						<a href="<?php echo base_url('maintenance/reloadscript'); ?>"><button type="button" class="btn btn-warning <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Reload Scripts</button></a>
-						<a href="<?php echo base_url('maintenance/reloadbattleconf'); ?>"><button type="button" class="btn btn-warning <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Reload Battle Conf</button></a>
-						<a href="<?php echo base_url('maintenance/reloadatcommand'); ?>"><button type="button" class="btn btn-warning <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Reload @command</button></a>
+						<a href="<?php echo base_url('server/maintenance/start'); ?>"><button type="button" class="btn btn-success <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Start Server</button></a>
+						<a href="<?php echo base_url('server/maintenance/stop'); ?>"><button type="button" class="btn btn-danger <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Stop Server</button></a>
+						<a href="<?php echo base_url('server/maintenance/restart'); ?>"><button type="button" class="btn btn-warning <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Restart Server</button></a><br /><br />
+						<a href="<?php echo base_url('server/maintenance/updatefiles'); ?>"><button type="button" class="btn btn-info <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Update Files</button></a><br /><br />
+						<a href="<?php echo base_url('server/maintenance/reloadscript'); ?>"><button type="button" class="btn btn-warning <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Reload Scripts</button></a>
+						<a href="<?php echo base_url('server/maintenance/reloadbattleconf'); ?>"><button type="button" class="btn btn-warning <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Reload Battle Conf</button></a>
+						<a href="<?php echo base_url('server/maintenance/reloadatcommand'); ?>"><button type="button" class="btn btn-warning <?php if ($check_perm['servermaint'] == 0) { echo "disabled"; } ?>">Reload @command</button></a>
 					</div>
 				</div>
 			</div>
@@ -74,7 +67,7 @@
 			<div class="col-lg-12">
 				<div class="panel panel-default">
 					<div class="panel-heading">
-						<i class="fa fa-bar-chart-o fa-fw"></i> Last 15 lines Login Server <a href="<?php echo base_url('console/login'); ?>">console</a>
+						<i class="fa fa-bar-chart-o fa-fw"></i> Last 15 lines Login Server <a href="<?php echo base_url('server/console/login'); ?>">console</a>
 					</div>
 					<div class="panel-body">
 						<?php echo nl2br($server_log['login']); ?>
@@ -84,7 +77,7 @@
 			<div class="col-lg-12">
 				<div class="panel panel-default">
 					<div class="panel-heading">
-						<i class="fa fa-bar-chart-o fa-fw"></i> Last 15 lines Char Server <a href="<?php echo base_url('console/char'); ?>">console</a>
+						<i class="fa fa-bar-chart-o fa-fw"></i> Last 15 lines Char Server <a href="<?php echo base_url('server/console/char'); ?>">console</a>
 					</div>
 					<div class="panel-body">
 						<?php echo nl2br($server_log['char']); ?>
@@ -94,7 +87,7 @@
 			<div class="col-lg-12">
 				<div class="panel panel-default">
 					<div class="panel-heading"> 
-						<i class="fa fa-bar-chart-o fa-fw"></i> Last 15 lines Map Server <a href="<?php echo base_url('console/map'); ?>">console</a>
+						<i class="fa fa-bar-chart-o fa-fw"></i> Last 15 lines Map Server <a href="<?php echo base_url('server/console/map'); ?>">console</a>
 					</div>
 					<div class="panel-body">
 						<?php echo nl2br($server_log['map']); ?>
