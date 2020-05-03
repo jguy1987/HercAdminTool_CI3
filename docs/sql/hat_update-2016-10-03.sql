faa0acc822c5ce85eb80860ea3a460b042c82e60
John Mish <jmish@jemstuff.com>
2020-05-03 15:22:05 -0500

    Docs commit.

--- docs/sql/hat_update-2016-10-03.sql
+++
@@ -1,8 +0,0 @@
--- HAT Table update.
--- 2016-10-03 @ 19:52 UTC
-
-ALTER TABLE `hat_users`
-	ADD `vacation` TINYINT(1) NOT NULL DEFAULT 0,
-	ADD `vacationsince` DATETIME NULL;
-
-
--- docs/sql/hat_update-2019-01-12.sql
+++
@@ -1,5 +0,0 @@
--- HAT Table update.
--- 2019-01-12 @ 14:31 UTC
-
-ALTER TABLE `hat_adminnews`
-	ADD `pinned` TINYINT(1) NOT NULL DEFAULT 0;
